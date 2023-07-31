b7869359fcb0388c7a0102c670209979167bbf41
seowon1112 <140315259+seowon1112@users.noreply.github.com>
2023-07-31 23:57:19 +0900

    2 files modified

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Typography from '@mui/material/Typography';
 
 export default function Main() {
   const [open, setOpen] = React.useState(false);
@@ -22,38 +23,45 @@ export default function Main() {
     setOpen(false);
   };
 
-  // 엔터 키를 눌렀을 때 Dialog를 여는 함수 추가
-const handleKeyDown = (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault(); // 기본 동작 취소
-    handleClickOpen();
-  }
-};
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // 기본 동작 취소
+      handleClickOpen();
+    }
+  };
 
-
-  // 처음 컴포넌트가 렌더링 될 때 keydown 이벤트를 추가하고 컴포넌트가 사라질 때 이벤트를 제거하는 useEffect 추가
   React.useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
-
+  
   return (
-    <div>
-     
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            backgroundColor: 'transparent',
-            boxShadow: 'none',
-          },
-        }}
-      >
-        <SignIn />
-      </Dialog>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', height: '100vh' }}>
+      <div>
+        {/* "취약" 제목 추가 */}
+        <Typography variant="h4" component="h1" align="center" gutterBottom style={{ marginTop: 150 }} fontSize="89.4pt" color="white">
+          取취약
+        </Typography>
+        {/* "백신" 부제 추가 */}
+        <Typography variant="h5" component="h2" align="center" fontSize="35pt" color="white">
+          백신을 취하다.
+        </Typography>
+
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              backgroundColor: 'transparent',
+              boxShadow: 'none',
+            },
+          }}
+        >
+          <SignIn />
+        </Dialog>
+      </div>
     </div>
   );
 }
